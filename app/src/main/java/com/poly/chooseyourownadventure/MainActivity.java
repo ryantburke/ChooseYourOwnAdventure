9d143cdc92f05ca2804bbf833fa2a29921ff549c
RBurke <ryanburk3@gmail.com>
2024-01-23 15:55:20 -0800

    Initial commit

---
+++ app/src/main/java/com/poly/chooseyourownadventure/MainActivity.java
@@ -0,0 +1,25 @@
+package com.poly.chooseyourownadventure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvStoryTitle, tvOptionTitle;
+    private ImageView ivStory;
+    private Button btn1, btn2, btn3, btn4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, AdventureLaborDay.class);
+
+        startActivity(intent);
+    }
+}
